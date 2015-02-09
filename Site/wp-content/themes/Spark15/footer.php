57bcd1f51b7571844f5edad1ec85cec4067c8319
Damon <damonjhastins@gmail.com>
2015-02-09 11:16:22 -0800

    New Templates 2/8

--- Site/wp-content/themes/Spark15/footer.php
+++ Site/wp-content/themes/Spark15/footer.php
@@ -2,40 +2,12 @@
 
 	$footerContext = Timber::get_context();
 	
-	/// Header
-	$footerContext['header'] = array(
-		'heading' 		=> get_option( 'footer_heading')
-		,'sub_heading'	=> get_option( 'footer_sub_heading')
-	);
-
-	/// Content Area
-	$twitterContext = array( 
-		'widget'	=> Timber::get_widgets('social')
-		,'link' 	=> get_option( 'twitter_link') 
-	);
-	$footerContext['twitter'] = Timber::compile('/views/components/twitter_block.html.twig', $twitterContext);
-	
-	$petitionContext = array(
-		'heading'	=> get_option('petition_heading')
-		,'text'		=> get_option('petition_text')
-		,'link'		=> get_option('petition_link')
-	);
-	$footerContext['petition'] = Timber::compile('/views/content/petition_block.html.twig', $petitionContext);
-
-	$footerContext['newsletter'] = Timber::compile('/views/components/newsletter_block.html.twig');//, $newsletterContext);
-	
 
 	/// Contact Area
-	$contactContext = array( 'nav' => new TimberMenu('Footer Contact') );
-	$footerContext['contact_menu'] = Timber::compile('views/components/nav.html.twig', $contactContext);
+	$sitemapContext = array( 'nav' => new TimberMenu('Main Nav') );
+	$footerContext['contact_menu'] = Timber::compile('views/components/nav.html.twig', $sitemapContext);
 
-	$socialContext = array( 
-		'twitter'	=> get_option('twitter_link')
-		,'facebook'	=> get_option('facebook_link')
-		,'email'	=> get_option('email_link')
-	);
-	$footerContext['social_menu'] = Timber::compile('views/components/social.html.twig', $contactContext);
-
+	$footerContext['social_menu'] = Timber::get_widgets('social_links');
 
 	Timber::render('views/content/footer.html.twig', $footerContext);
 
